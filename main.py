7498af9427c218092afb9a85a6c276e9de5429c1
amin rabinia <amin.rabinia@gmail.com>
2023-02-10 12:44:19 -0800

    "updated model and main.py"

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from ultralytics import YOLO
 import uvicorn
 import gradio as gr
-from PIL import Image
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
 
-model = YOLO('best.pt') 
+model = YOLO('last.pt') 
 
 
 app = FastAPI()
@@ -22,20 +21,25 @@ app.add_middleware(
 
 @app.get('/')
 def root():
-    return {"message": "hello!"}
+    return {"message": "hello from image segmentation!"}
 
 
-def gradio_infer(myimg, conf=0.50):
-    if myimg is not None:
-    	results = model.predict(myimg, device='cpu', conf=conf, show=True)
-    	return results
+async def gradio_infer(myimg, conf=0.50):
+    if myimg is None:
+        return
+    results = model.predict(myimg, device='cpu', conf=conf, save=True) 
+    print(results)
+    if len(results[0].numpy())==0:
+        return myimg
+    else:
+        return model.predictor.annotator.result()
 
 
 io = gr.Interface(fn=gradio_infer, 
 	inputs=[gr.Image(), 
 			gr.Slider(0.20, 0.80, step=0.10, value=0.50, label="Set the confidence level:")],
 	outputs=gr.Image(),
-	examples=[["testimg.jpg", 0.50], ["testimg2.jpg", 0.60]],
+	examples=[["testimg.jpg", 0.30], ["testimg2.jpg", 0.60]],
 	allow_flagging = 'never',
 	css="footer {visibility: hidden}",
 	live=True
